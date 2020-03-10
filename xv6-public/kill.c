762c19e35f339e272dc66ab3cc1c7b6b6767f1d3
Santiago Aragon <santiagoas77@gmail.com>
2020-03-10 23:34:58 +0000

    Practica 3

--- xv6-public/kill.c
+++ xv6-public/kill.c
@@ -11,7 +11,10 @@ main(int argc, char **argv)
     printf(2, "usage: kill pid...\n");
     exit();
   }
-  for(i=1; i<argc; i++)
-    kill(atoi(argv[i]));
+  for(i=1; i<argc; i++){
+    if(atoi(argv[i])!=1){
+        kill(atoi(argv[i]));
+    }
+  }
   exit();
 }
